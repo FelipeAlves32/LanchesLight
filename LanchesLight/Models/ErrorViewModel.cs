c73341fa6753960258262d8bdd6c25b1e1060905
FelipeAlves32 <felipe_alves.26@hotmail.com>
2022-06-07 23:11:57 -0300

    Add project files.

---
+++ LanchesLight/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesLight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
